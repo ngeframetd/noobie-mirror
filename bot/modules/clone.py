5f4da376ddc9645aedc13f6f795997586c2344aa
ngeframetd <70860536+ngeframetd@users.noreply.github.com>
2022-09-18 16:37:39 +0700

    Update clone.py

--- bot/modules/clone.py
+++ bot/modules/clone.py
@@ -111,7 +111,7 @@ def _clone(message, bot):
                 pass
         if _msg:
             deleteMessage(bot, _msg)
-        cc = f'\n\n<b>Time:</b> {get_readable_time(time() - message.date.timestamp())}'
+        cc = f'\n<b>Finished in:</b> {get_readable_time(time() - message.date.timestamp())}'
         cc += f'\n\n<b>cc:</b> {tag}'
         if not reply_to or reply_to.from_user.is_bot:
             cc += f'\n<b>UID: </b><code>{message.from_user.id}</code>'
